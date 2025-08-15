9abe5277523bd53a94e45399a958d035a1d11a0c
law <theunknownlawrence@gmail.com>
2025-08-15 13:23:04 +1000

    removed qts logo from top left

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,12 +3,13 @@ import React from "react";
 export const Header = ({ logo, circularLogo, title, subtitle }) => {
     return (
         <div style={{ textAlign: "center" }}>
-            <img
+            {/* REMOVED QTS LOGO TEMPORARILY */}
+            {/* <img
                 src={logo}
                 className="logo"
                 style={{ borderRadius: circularLogo ? "100vw" : 0 }}
                 alt="logo"
-            />
+            /> */}
             <h1>{title}</h1>
             <h2>{subtitle}</h2>
         </div>
